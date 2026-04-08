72543c7b4a0b54c314f98dd6e4e08261c8482bb0
BeginnerCoder52 <126638696+BeginnerCoder52@users.noreply.github.com>
2026-04-09 01:22:54 +0700

    init usb camera_test

---
+++ frt_app/cpp_camera_core/include/UvcStillMagic.hpp
@@ -0,0 +1,11 @@
+#ifndef UVC_STILL_MAGIC_HPP
+#define UVC_STILL_MAGIC_HPP
+
+/**
+ * @brief Still image format magic from the custom uvc driver
+ * 
+ * Used for still image capture support in the custom uvc kernel module.
+ */
+#define UVC_STILL_MAGIC 0xF03E5335
+
+#endif // UVC_STILL_MAGIC_HPP
