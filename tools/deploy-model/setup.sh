2ad571269c8a810210c15c4956dee0c4ae1ff0f5
dien-github <145799386+dien-github@users.noreply.github.com>
2026-04-12 18:18:30 +0000

    [U] test(inference): Enhance setup script with model downloads and performance options

--- tools/deploy-model/setup.sh
+++ tools/deploy-model/setup.sh
@@ -1,13 +1,91 @@
 #!/usr/bin/env bash
 
-#
-# Download models
-#
+usage() {
+    echo "Use: $0 [--disable-swap] [--force-cpu]"
+    echo "  --disable-swap  Disable swap file and config zram"
+    echo "  --force-cpu     Set performance mode for CPU"
+    exit 1
+}
 
-mkdir -p ./models
-cd ./models
-MODEL_INT8_URL = "https://github.com/BeginnerCoder52/FSS/releases/download/v0.1.0-alpha/best_int8.tflite"
-MODEL_FP32_URL = "https://github.com/BeginnerCoder52/FSS/releases/download/v0.1.0-alpha/best_float32.tflite"
-echo "[+] Downloading models..."
-wget -O models/model_int8.tflite "$MODEL_INT8_URL"
-wget -O models/model_fp32.tflite "$MODEL_FP32_URL"
+if [ $# -eq 0 ]; then
+    #
+    # Update and Install packages
+    #
+    sudo apt update && sudo apt full-upgrade -y # Cập nhật các gói và nâng cấp hệ thống
+    sudo apt install -y htop zram-tools linux-cpupower curl wget git
+
+    #
+    # Download models
+    #
+    mkdir -p ./models
+    cd ./models
+    MODEL_INT8_URL="https://github.com/BeginnerCoder52/FSS/releases/download/v0.1.0-alpha/best_int8.tflite"
+    MODEL_FP32_URL="https://github.com/BeginnerCoder52/FSS/releases/download/v0.1.0-alpha/best_float32.tflite"
+    echo "[+] Downloading models..."
+    wget -O model_int8.tflite "$MODEL_INT8_URL"
+    wget -O model_fp32.tflite "$MODEL_FP32_URL"
+
+    #
+    # Disable unnecessary services
+    #
+    sudo systemctl disable bluetooth # Tắt bluetooth service không cần thiết sử dụng Bluetooth
+    sudo systemctl disable hciuart # Tắt hciuart service không cần thiết sử dụng Bluetooth
+    sudo systemctl disable ModemManager.service # Tắt Modem Manager vì không cần sử dụng modem dữ liệu di động
+fi
+
+for arg in "$@"; do
+    case $arg in
+        --disable-swap)
+            #
+            # Disable swap file, change to zram
+            #
+            echo "[+] Disabling swap and enabling zram..."
+            sudo apt purge -y dphys-swapfile # Xóa hoàn toàn phần tương tác file swap
+            sudo rm -f /var/swap # Tắt swap file để bảo vệ thẻ nhớ
+            sudo bash -c 'cat <<EOF > /etc/default/zramswap
+ALGO=lz4
+PERCENT=50
+EOF'
+            sudo systemctl restart zramswap # Tái khởi tạo zram nhằm tối ưu bộ nhớ
+
+            shift
+            ;;
+        --force-cpu)
+            #
+            # Force CPU performance
+            #
+            sudo bash -c 'cat <<EOF > /etc/systemd/system/cpupower-performance.service
+[Unit]
+Description=Set CPU governor to performance
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/cpupower frequency-set -g performance
+
+[Install]
+WantedBy=multi-user.target
+EOF' # Tạo một service để khởi động mỗi khi khởi động
+            sudo systemctl daemon-reload # Reload daemon
+            sudo systemctl enable cpupower-performance.service # Mở service mới viết
+            sudo systemctl start cpupower-performance.service # Khởi động service mới tạo
+
+            BOOT_CONFIG="/boot/firmware/config.txt"
+            if [ ! -f "$BOOT_CONFIG" ]; then
+                BOOT_CONFIG="/boot/config.txt"
+            fi
+            if ! grep -q "arm_freq=1350" "$BOOT_CONFIG"; then
+            sudo bash -c "cat <<EOF > \"$BOOT_CONFIG\"
+arm_freq=1350
+core_freq=500
+over_voltage=4
+temp_limit=80
+EOF"
+            fi
+            shift
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
